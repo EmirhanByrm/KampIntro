240bf46abbc8154d06533bf0195910ebb36c18ef
Emirhan bayram <FakeLifeTk@outlook.com>
2023-08-27 16:54:45 +0300

    ClassMetotDemo Güncellemesi Ders 3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void CustomerAdd(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" "+musteri.Soyad+" "+musteri.Id+" "+"Eklendi");
+        }
+        public void CustomerDelete(Musteri musteri) 
+        { Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " " + musteri.Id + " " + "Silindi"); }  
+        public void CustomerUpdate(Musteri musteri) 
+        { Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " " + musteri.Id + " " + "Güncellendi"); }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri user = new Musteri();
+            user.Ad = "Emirhan";
+            user.Soyad = "Bayram";
+            user.Id = 1;
+            Musteri user2 = new Musteri();
+            user2.Ad = "Aslı";
+            user2.Soyad = "Deniz";
+            user2.Id = 2;
+            Musteri user3 = new Musteri();
+            user3.Ad = "Hakan";
+            user3.Soyad = "Pak";
+            user3.Id = 3;
+
+            Musteri[] users = new Musteri[] { user, user2, user3 };
+            foreach (Musteri info in users)
+            {
+                Console.WriteLine(info.Ad);
+                Console.WriteLine(info.Soyad);
+                Console.WriteLine(info.Id);
+            }
+
+            MusteriManager method = new MusteriManager();
+            /*ADD*/
+            method.CustomerAdd(user);
+            method.CustomerAdd(user2);
+            method.CustomerAdd(user3);
+            /*UPDATE*/
+            method.CustomerUpdate(user);
+            method.CustomerUpdate(user2);
+            method.CustomerUpdate(user3);
+            /*DELETE*/
+            method.CustomerDelete(user);
+            method.CustomerDelete(user2);
+            method.CustomerDelete(user3);
+            
+
+        }
+    }
+}
